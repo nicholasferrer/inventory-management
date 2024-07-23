cd0064bb4a8711d9f330c1e92cef582b65cb97b4
Nicholas Ferrer <nferrer@americastg.com>
2024-07-23 17:33:33 -0300

    add sign-up page and header

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 export function AuthLayout() {
   return (
     <>
-      <div className="grid min-h-screen grid-cols-2">
+      <div className="grid min-h-screen grid-cols-2 antialiased">
         <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
           <div className="flex items-center gap-3 text-lg text-foreground">
             <Coffee className="h-5 w-5" />
@@ -16,7 +16,7 @@ export function AuthLayout() {
           </footer>
         </div>
 
-        <div className="flex flex-col items-center justify-center">
+        <div className="relative flex flex-col items-center justify-center">
           <Outlet />
         </div>
       </div>
